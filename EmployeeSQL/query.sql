6fe6022da94fb29080a4f13ca5127f1a5329a079
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-07-31 14:33:51 -0400

    Added comment on code

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,3 +1,4 @@
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT e.emp_no AS "Employee Number",
 	     e.last_name AS "Last Name",
 	     e.first_name AS "First Name",
@@ -8,7 +9,7 @@ INNER JOIN salaries AS s
   ON e.emp_no = s.emp_no;
 
 
-
+-- List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name AS "First Name",
 	     last_name AS "Last Name",
 	     hire_date AS "Hire Date"
@@ -16,7 +17,7 @@ FROM employees
 WHERE EXTRACT (YEAR FROM hire_date) = '1986';
 
 
-
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT d.dept_no AS "Department Number",
 	     d.dept_name AS "Department Name",
 	     dm.emp_no AS "Employee Number",
@@ -29,7 +30,7 @@ INNER JOIN employees AS e
 	ON dm.emp_no = e.emp_no;
 
 
-
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
 SELECT e.emp_no AS "Employee Number",
   		 e.last_name "Last Name",
   		 e.first_name "First Name",
@@ -41,7 +42,7 @@ INNER JOIN departments AS d
   ON de.dept_no = d.dept_no;
 
 
-
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name AS "First Name",
 		   last_name AS "Last Name",
 		   sex AS "Sex"
@@ -63,6 +64,7 @@ WHERE first_name = 'Hercules' AND
 -- WHERE d.dept_name = 'Sales';
 
 
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT e.emp_no AS "Employee Number",
        e.last_name AS "Last Name",
        e.first_name AS "First Name",
@@ -74,7 +76,7 @@ FROM employees AS e;
 
 
 
-
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no AS "Employee Number",
 		   e.last_name AS "Last Name",
 		   e.first_name AS "First Name",
@@ -88,7 +90,7 @@ WHERE d.dept_name = 'Sales' OR
       d.dept_name = 'Development';
 
 
-
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name AS "Last Name",
        count(last_name) AS "Count of employees last name"
 FROM employees
